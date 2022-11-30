af235db6929da2456e28a981042aa54b2ade0c58
Quintion <QuintionTang@gmail.com>
2022-11-30 11:46:57 +0800

    feat:增加数据库部分逻辑

--- README.md
+++ README.md
@@ -16,6 +16,18 @@ npm install
 npm run dev
 ```
 
+### DATABASE
+
+使用 Docker 启动一个数据库实例，关于 Docker 的介绍，建议参阅《[面向 WEB 开发人员的 Docker](https://juejin.cn/column/6965049243660714021)》。
+
+```
+docker run -it -v $PWD/mongodata:/data/db -p 27017:27017 --name mongodb -d mongo
+```
+
+### 相关文章
+
+-   [Nodejs、GraphQL、MongoDB、Hapi 和 Swagger 构建 API（一）](https://www.devpoint.cn/article/441.shtml)
+
 ### 个人自媒体
 
 -   个人网站：[https://www.devpoint.cn/index.shtml](https://www.devpoint.cn/index.shtml)
--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 const hapi = require("hapi");
-
+const mongoose = require("mongoose");
 const server = hapi.server({
     port: 3006,
     host: "localhost",
 });
 
+const mongoUrl = "mongodb://127.0.0.1:27018/powerful";
+const mainDB = mongoose
+    .createConnection(mongoUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .asPromise();
+
+mainDB
+    .then((db) => {
+        console.info("Connected to MongoDB mainDB");
+        db.model("Users", new mongoose.Schema({ name: String }));
+    })
+    .catch((err) => {
+        console.error("Failed to connect to mainDB", {
+            params: { err: err.message },
+        });
+    });
+
 const init = async () => {
+    server.route({
+        method: "GET",
+        path: "/",
+        handler: (request, response) => `<h1>This is powerful api.</h1>`,
+    });
+
     await server.start();
     console.log(`Server running at:${server.info.uri}`);
 };
